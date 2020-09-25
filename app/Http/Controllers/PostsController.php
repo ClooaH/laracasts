960e5cac5fd7c01061664b1513640f60905c4f6d
ClooaH <a.s.d.w.a.s@hotmail.com>
2020-09-25 12:54:32 +0200

    ep.10

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
@@ -15,10 +15,10 @@ class PostsController extends Controller
     
     public function show($slug) {
 
-        $post = \DB::table('posts')->where('slug', $slug)->first();
-    
+    //    $post = \DB::table('posts')->where('slug', $slug)->first();
+
         return view('post', [
-            'post' => $post
+            'post' => Post::where('slug', $slug)->firstOrFail()
         ]);
     }
 }
