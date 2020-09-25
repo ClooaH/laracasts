34269677f62d67db308d5267c287de9e0b17d746
ClooaH <a.s.d.w.a.s@hotmail.com>
2020-09-25 10:17:51 +0200

    ep.7

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,20 @@ Route::get('/', function () {
         'name' => $name
     ]);
 });
+
+Route::get('/posts/{post}', function ($post) {
+    
+    $posts = [
+        'yes1' => 'my first yes ever',
+        'yes2' => 'second yes',
+        'yes3' => 'third...?'
+    ];
+
+    if (!array_key_exists($post, $posts)) {
+        abort(404, 'sorry, not found');
+    }
+    
+    return view('post', [
+        'post' => $posts[$post]
+    ]);
+});
